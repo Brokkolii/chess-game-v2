2b1147cd4b02a1af39b6f35fd1cbd28e0b9b1e00
Patrick Girnghuber <patrick.girnghuber@icloud.com>
2024-07-29 22:26:04 +0200

    #25: bot choosing random moves

--- bot/bot.go
+++ bot/bot.go
@@ -1,6 +1,8 @@
 package bot
 
 import (
+	"math/rand"
+
 	"github.com/Brokkolii/chess-game-v2/board"
 	"github.com/Brokkolii/chess-game-v2/match"
 )
@@ -8,18 +10,23 @@ import (
 func GetMove(state *match.State) *board.Move {
 
 	moves := state.Board.MovesForColor(state.Board.Turn, false)
-	var consideredMove *board.Move
+	var consideredMoves []*board.Move
 	var consideredMoveEval int
 
 	for _, move := range moves {
 		b := state.Board.DeepCopy()
 		b.ExecuteMove(move)
 		eval := b.Evaluate()
-		if eval >= consideredMoveEval {
-			consideredMove = move
+		if eval > consideredMoveEval {
+			// only this one move in array
+			consideredMoves = append([]*board.Move{move}, nil)
 			consideredMoveEval = eval
+		} else if eval == consideredMoveEval {
+			// add to array
+			consideredMoves = append(consideredMoves, move)
 		}
 	}
 
-	return consideredMove
+	// chose one of the best Moves
+	return consideredMoves[rand.Intn(len(consideredMoves))] //at random positon
 }
